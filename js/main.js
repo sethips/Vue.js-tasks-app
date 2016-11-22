48a6bb06da80e354e824f3e6f0d649f13f85bf43
Surbhi Oberoi <siyaoberoi93@gmail.com>
2016-11-22 15:35:14 +0530

    work in progress- vue js app for tasks completed and in progress

---
+++ js/main.js
@@ -0,0 +1,36 @@
+Vue.component('tasks', {
+    template: '#tasks-template',
+    props: ['list'],
+    computed: {
+        remaining: function() {
+            var vm = this;
+            return this.list.filter(this.inProgress).length;
+        }
+    },
+
+    methods: {
+        isCompleted: function(task) {
+            return task.completed;
+        },
+        inProgress: function(task) {
+            return !this.isCompleted(task);
+        },
+        deleteTask: function(task) {
+            this.list.$remove(task);
+        },
+        clearCompleted: function() {
+            this.list = this.list.filter(this.inProgress);
+        }
+    }
+})
+
+new Vue({
+    el: '#app',
+    data: {
+        tasks: [
+            { body: 'Go to the store', completed: false },
+            { body: 'Go to the store', completed: false },
+            { body: 'Go to the store', completed: true }
+        ]
+    }
+})
